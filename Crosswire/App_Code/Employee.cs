1e0d987476950635a94b1734f6153b2ffdcbf15f
Hamish Cumming <hamish.cumming@gmail.com>
2018-04-13 15:03:02 +1200

    updates to extract callsign data from the database

---
+++ Crosswire/App_Code/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Employee
+/// </summary>
+public class Employee
+{
+    public string Name { get; set; }
+    public int Callsign { get; set; }
+    public string EmployeeNumber { get; set; }
+}
